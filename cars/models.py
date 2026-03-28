90ee5c9a7cf60a594e4e7270f33bfce9ebd1d8ec
Roman Martynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2026-03-28 22:26:42 +0200

    feat: add Car and SearchRequest models

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    mileage = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.brand} {self.model} {str(self.year)}"
+
+class SearchRequest(models.Model):
+    max_price = models.DecimalField(decimal_places=2, max_digits=10)
+    min_year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
