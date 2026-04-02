04a0a5d54614e303f92ba7b13fc0ea883ec8bf0a
Roman Martynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2026-04-02 21:50:01 +0300

    feat: add car filtering by price and year

--- cars/views.py
+++ cars/views.py
@@ -8,5 +8,15 @@ from cars.serializers.car_serializer import CarSerializer
 class CarListView(APIView):
     def get(self, request):
         cars = Car.objects.all()
+
+        max_price = request.GET.get('max_price')
+        min_year = request.GET.get('min_year')
+
+        if max_price:
+            cars = cars.filter(price__lte=max_price)
+
+        if min_year:
+            cars = cars.filter(year__gte=min_year)
+
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
