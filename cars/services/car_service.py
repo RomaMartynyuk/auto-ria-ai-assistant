9dfe2d86e54a682a8f3cdf9d8165467dd545a3f0
Roman Martynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2026-04-06 18:36:38 +0300

    feat: add caching for car filtering + logging

--- cars/services/car_service.py
+++ cars/services/car_service.py
@@ -1,6 +1,19 @@
 from cars.models import Car
+from django.core.cache import cache
+import logging
+
+logger = logging.getLogger(__name__)
 
 def filter_cars(max_price=None, min_year=None):
+    cache_key = f"cars_{max_price}_{min_year}"
+
+    cached_data = cache.get(cache_key)
+    if cached_data:
+        logger.info("FROM CACHE")
+        return cached_data
+
+    logger.info("FROM DB")
+
     cars = Car.objects.all()
 
     if max_price:
@@ -8,5 +21,9 @@ def filter_cars(max_price=None, min_year=None):
 
     if min_year:
         cars = cars.filter(year__gte=min_year)
+
+    cars = list(cars)
+
+    cache.set(cache_key, cars, timeout=60 * 5)
 
     return cars
--- config/settings.py
+++ config/settings.py
@@ -111,6 +111,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Logging
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
