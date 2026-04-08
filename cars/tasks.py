7c1395f80729c2c7e4fcc4394b7e73fc3cad0102
Roman Martynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2026-04-08 21:08:39 +0300

    feat: integrate celery task into car recommendation endpoint

--- cars/tasks.py
+++ cars/tasks.py
@@ -8,3 +8,15 @@ def test_task():
     print("Task executed")
 
     return "done"
+
+@shared_task
+def process_car_search(max_price, min_year):
+    print("START PARSING / PROCESSING")
+
+    time.sleep(5)
+
+    print(f"Filters: price={max_price}, year={min_year}")
+
+    print("END TASK")
+
+    return {"status": "done"}
--- cars/views.py
+++ cars/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from cars.models import Car
 from cars.serializers.car_serializer import CarSerializer
 from cars.services.car_service import filter_cars
+from cars.tasks import process_car_search
 
 
 class CarListView(APIView):
@@ -21,8 +22,9 @@ class CarRecommendView(APIView):
         max_price = request.GET.get('max_price')
         min_year = request.GET.get('min_year')
 
-        cars = filter_cars(max_price, min_year)
+        process_car_search.delay(max_price, min_year)
 
+        cars = filter_cars(max_price, min_year)
         cars = sorted(cars, key=lambda x: x.year, reverse=True)[:5]
 
         serializer = CarSerializer(cars, many=True)
